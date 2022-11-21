1ad028fa950b1fa16f8b12b201721d6a73c6ce64
aleks <olabodziacka@op.pl>
2022-11-21 22:01:30 +0100

    pierwsze usuwanie niepotrzebnych komentarzy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,9 @@ import { Component,ViewEncapsulation, OnInit, Inject } from '@angular/core';
 import {AfterViewInit, ViewChild} from '@angular/core';
 import {MatPaginator} from '@angular/material/paginator';
 import {MatTableDataSource} from '@angular/material/table';
-import {MatPaginatorModule} from '@angular/material/paginator';
-import {HttpClient, HttpHeaders} from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { catchError } from 'rxjs/operators';
 import { OrdersService } from './orders.service';
-import { Items, Orders } from './models';
-import {MatDialog, MAT_DIALOG_DATA} from '@angular/material/dialog';
-import { Dialog } from '@angular/cdk/dialog';
+import {Orders } from './models';
+import {MatDialog} from '@angular/material/dialog';
 import { DialogAppComponent } from './dialog-app/dialog-app.component';
 
 
@@ -35,24 +30,20 @@ export class AppComponent implements AfterViewInit {
 
   getOrders(){
     this.ordersService.getOrders().subscribe((data)=>{this.orders=data
-    console.log(data)
     this.dataSource.data = this.orders;
-    console.log(this.dataSource);})
+    })
   }
 
   gerFirstProduct(element: Orders) {
     return element.line_items[0].name + ' x ' + element.line_items[0].quantity + "...";
   }
 
-
   @ViewChild(MatPaginator) paginator: MatPaginator;
 
   ngAfterViewInit() {
     this.dataSource=new MatTableDataSource(this.orders);
-    console.log(this.paginator)
     this.dataSource.paginator = this.paginator;
     setTimeout(() => this.dataSource.paginator = this.paginator);
-    console.log(this.dataSource);
   }
 
   openDialog(id: number){
--- src/app/dialog-app/dialog-app.component.ts
+++ src/app/dialog-app/dialog-app.component.ts
@@ -2,8 +2,6 @@ import { Component, OnInit, Inject } from '@angular/core';
 import {MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { MatTableDataSource } from '@angular/material/table';
 import { Orders, Shipping } from '../models';
-import { OrdersService } from '../orders.service';
-import { Product } from '../products';
 import { ProductsService } from '../products.service';
 
 export interface DialogData {id: number}
@@ -17,8 +15,6 @@ export class DialogAppComponent implements OnInit {
 
   orders: any;
   dataSource = new MatTableDataSource<any>();
-  Products: Array<Product>=[];
-  product = new Product();
   order: Orders;
   shippingDetails: Array<Shipping>;
 
@@ -32,7 +28,6 @@ export class DialogAppComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.addProduct();
     this.productsService.getProducts(this.data.id).subscribe(order=>{
       this.order=order;
       console.log(order)});
@@ -41,24 +36,6 @@ export class DialogAppComponent implements OnInit {
 
   getProducts(element: Orders){
     return element?.line_items[0].name;
-    // return element.shipping;
-    // return element.shipping.forEach(item=>{
-    //   item.address_1;
-    // })
-   // this.shippingDetails=this.order.shipping;
   }
 
-// }
-
-  addProduct(){
-
-    // this.ordersService.getOrders().subscribe((data)=>{this.orders=data
-    //   console.log(data)
-
-    // this.product.id=this.data;
-    // this.product.name=this.orders.line_items.name;
-    // this.product.quantity=this.orders.line_items.quantity;
-    // this.Products.push(this.product);
-    // console.log(this.Products);})
-  }
 }
